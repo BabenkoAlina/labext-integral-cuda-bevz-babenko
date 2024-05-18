ad0791f5f99e422c71060960d5aee84cca6258d5
ybevz <bevz.pn@ucu.edu.ua>
2024-05-18 16:44:04 +0300

    Configs created, header for functions written

---
+++ includes/functions.hpp
@@ -0,0 +1,9 @@
+#ifndef INTEGRAL_CUDA_FUNCTIONS_HPP
+#define INTEGRAL_CUDA_FUNCTIONS_HPP
+
+double function_1(double x, double y);
+double function_2(double x, double y);
+double function_3(double x, double y);
+double function_4(double x, double y);
+
+#endif //INTEGRAL_CUDA_FUNCTIONS_HPP
